ae6b760acbef9e8053fdcf79f86592d16ed638cf
Annitas <39757257+Annitas@users.noreply.github.com>
2023-11-27 14:25:04 +0600

    Make project structure

---
+++ JuniorMathFac/JuniorMathFac/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JuniorMathFac
+//
+//  Created by Anita Stashevskaya on 20.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
